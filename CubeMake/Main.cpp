06f70c951dba57481245dbe89248885ac6249e40
konstantin ttv <titov.k.21@bk.ru>
2020-03-11 18:16:40 +0300

    jj

--- CubeMake/Main.cpp
+++ CubeMake/Main.cpp
@@ -1,10 +1,33 @@
-#include <SOIL.h>
+//#include <SOIL.h>
 #include <glut.h>
 #include <iostream>
 
-void main() {
-	std::cout << "Its working!" << std::endl;
-	std::cout << "Its working!111" << std::endl;
-	printf("privet");
-	std::cout << "ssssssssssss";
+void renderScene(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.0, 0.5, 0.0);
+	glVertex3f(0.5, -0.5, 0.0);
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[]) {
+
+	// инициализация
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("Урок 1");
+
+	// регистрация обратных вызовов
+	glutDisplayFunc(renderScene);
+
+	// Основной цикл GLUT
+	glutMainLoop();
+
+	return 1;
 }
