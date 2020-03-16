3aa5be674aa5b474839b2c7946ff89f29e85e860
konstantin ttv <titov.k.21@bk.ru>
2020-03-17 00:38:04 +0300

    refresh3

--- CubeMake/Main.cpp
+++ CubeMake/Main.cpp
@@ -2,31 +2,63 @@
 #include <glut.h>
 #include <iostream>
 
+float angle = 0.0f;
+
+void changeSize(int w, int h) {
+	// предотвращение деления на ноль
+	if (h == 0)
+		h = 1;
+	float ratio = w * 1.0 / h;
+	// используем матрицу проекции
+	glMatrixMode(GL_PROJECTION);
+	// обнуляем матрицу
+	glLoadIdentity();
+	// установить параметры вьюпорта
+	glViewport(0, 0, w, h);
+	// установить корректную перспективу
+	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
+	// вернуться к матрице проекции
+	glMatrixMode(GL_MODELVIEW);
+}
+
 void renderScene(void) {
+	// очистка буфера цвета и глубины
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// обнуление трансформации
+	glLoadIdentity();
+	// установка камеры
+	gluLookAt(0.0f, 0.0f, 10.0f,
+		0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f);
+
+	glRotatef(angle, 0.0f, 1.0f, 0.0f);
 
 	glBegin(GL_TRIANGLES);
-	glVertex3f(-0.5, -0.5, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glVertex3f(0.5, -0.5, 0.0);
+	glVertex3f(-2.0f, -2.0f, 0.0f);
+	glVertex3f(0.0f, 2.0f, 0.0);
+	glVertex3f(2.0f, -2.0f, 0.0);
 	glEnd();
+
+	angle += 0.1f;
 
 	glutSwapBuffers();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char** argv) {
 
-	// инициализация
+	// Инициализация GLUT и создание окна
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(400, 400);
-	glutCreateWindow("Урок 1");
+	glutCreateWindow("Урок 3");
 
-	// регистрация обратных вызовов
+	// регистрация
 	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
 
-	// Основной цикл GLUT
+	// основной цикл
 	glutMainLoop();
 
 	return 1;
